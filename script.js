ba178b5e5ed1ae5588c3ee31e69a348b2343674b
ANAGH <anaghkrkkl@gmail.com>
2021-03-11 14:55:00 +0530

    script.js edit almost finish

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function display(num){
-    document.getElementById('screen').value=num;
+    document.getElementById('screen').value+=num;
 }
 function ac(){
-    document.getElementById('screen').value=0;
+    document.getElementById('screen').value="";
+}
+function solve(){
+    let num1=document.getElementById('screen').value;
+    let num2=eval(num1);
+    document.getElementById('screen').value=num2;
+}
+function del() {
+    var value = document.getElementById("screen").value;
+    document.getElementById("screen").value = value.substr(0, value.length - 1);
 }
